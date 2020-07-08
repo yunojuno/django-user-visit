c0561b0c0ca609e879f26fc70b82b28cbe276553
Hugo Rodger-Brown <hugorodgerbrown@users.noreply.github.com>
2020-07-08 18:31:39 +0100

    Remove session caching (#6)
    Caching visits in the request.session seems like a good idea, but it's causing problems under load - leading to a lot of django.db.IntegrityError warnings whilst trying to save duplicate hashes.
    
    Back to the route 1 approach - just look it up in the database. Adds a db read on each request. If that becomes an issue I'll revisit caching. Lesson learned again - always go simple.

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -4,44 +4,29 @@ import django.db
 import freezegun
 import pytest
 from django.contrib.auth.models import User
-from django.contrib.sessions.backends.base import SessionBase
 from django.core.exceptions import MiddlewareNotUsed
 from django.http import HttpResponse
 from django.test import Client
+from django.utils import timezone
 
-from user_visit.middleware import (
-    SESSION_KEY,
-    UserVisitMiddleware,
-    cache_visit,
-    visit_is_cached,
-)
+from user_visit.middleware import UserVisitMiddleware, save_user_visit
 from user_visit.models import UserVisit, UserVisitManager
 
 
-class TestMiddlewareFunctions:
-    @pytest.mark.parametrize(
-        "hash_value,cached_value,result",
-        (
-            ("", "", False),
-            ("", "foo", False),
-            ("foo", "", False),
-            ("foo", "bar", False),
-            ("bar", "bar", True),
-        ),
+@pytest.mark.django_db
+def test_save_user_visit():
+    """Test standalone save method handles db.IntegrityError."""
+    user = User.objects.create(username="Yoda")
+    timestamp = timezone.now()
+    uv = UserVisit.objects.create(
+        user=user,
+        session_key="test",
+        ua_string="Chrome",
+        remote_addr="127.0.0.1",
+        timestamp=timestamp,
     )
-    def test_visit_is_cached(self, hash_value, cached_value, result):
-        session = {SESSION_KEY: cached_value}
-        visit = UserVisit(hash=hash_value)
-        assert visit_is_cached(visit, session) == result
-
-    @pytest.mark.parametrize("hash_value,cached", (("", False), ("bar", True),))
-    def test_cache_visit(self, hash_value, cached):
-        """Check that hash is not stored if empty."""
-        session = {}
-        visit = UserVisit(hash=hash_value)
-        assert SESSION_KEY not in session
-        cache_visit(visit, session)
-        assert (SESSION_KEY in session) == cached
+    uv.id = None
+    save_user_visit(uv)
 
 
 @pytest.mark.django_db
@@ -86,24 +71,13 @@ class TestUserVisitMiddleware:
             client.get("/")
             assert UserVisit.objects.count() == 2
 
-    def test_middleware__duplicate(self):
-        """Check that middleware handles duplicate uuids."""
-        user = User.objects.create_user("Fred")
-        client = Client()
-        client.force_login(user)
-        client.get("/")
-        with mock.patch("user_visit.middleware.visit_is_cached", return_value=False):
-            client.get("/")
-
     def test_middleware__db_integrity_error(self):
-        """Check that middleware stores hash when db raises duplicate hash error."""
+        """Check that a failing save doesn't kill middleware."""
         user = User.objects.create_user("Fred")
         client = Client()
         client.force_login(user)
-        assert not client.session.get(SESSION_KEY)
         with mock.patch.object(UserVisit, "save", side_effect=django.db.IntegrityError):
             client.get("/")
-            assert client.session[SESSION_KEY]
 
     @mock.patch("user_visit.middleware.RECORDING_DISABLED", True)
     def test_middleware__disabled(self):
