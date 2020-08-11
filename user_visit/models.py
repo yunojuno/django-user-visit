70870b31469e75444220128f6f761c0a30039a3e
Almas Baktubayev <a.baktubayev@gmail.com>
2020-08-11 10:35:24 +0600

    change id to pk in md5 function

--- user_visit/models.py
+++ user_visit/models.py
@@ -123,8 +123,8 @@ class UserVisit(models.Model):
 
     # see https://github.com/python/typeshed/issues/2928 re. return type
     def md5(self) -> hashlib._Hash:
-        """Generate MD5 hash used to identify duplicate visits."""
-        h = hashlib.md5(str(self.user.id).encode())  # noqa: S303
+        """Generate MD5 hash used to primary key duplicate visits."""
+        h = hashlib.md5(str(self.user.pk).encode())  # noqa: S303
         h.update(self.date.isoformat().encode())
         h.update(self.session_key.encode())
         h.update(self.remote_addr.encode())
