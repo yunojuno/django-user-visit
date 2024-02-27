af2a8924b83cfcceef567d09cffd80fa2285bd65
Paul Craciunoiu <paul@craciunoiu.net>
2024-02-26 19:10:10 -0700

    Add support for async middleware.

--- user_visit/middleware.py
+++ user_visit/middleware.py
@@ -1,10 +1,13 @@
+import inspect
 import logging
 import typing
 
 import django.db
+from asgiref.sync import sync_to_async
 from django.core.exceptions import MiddlewareNotUsed
 from django.http import HttpRequest, HttpResponse
 from django.utils import timezone
+from django.utils.decorators import sync_and_async_middleware
 
 from user_visit.models import UserVisit
 
@@ -24,23 +27,55 @@ def save_user_visit(user_visit: UserVisit) -> None:
         )
 
 
-class UserVisitMiddleware:
-    """Middleware to record user visits."""
+# Atomic transactions are not supported async yet
+# https://docs.djangoproject.com/en/5.0/topics/async/
+asave_user_visit = sync_to_async(save_user_visit)
 
-    def __init__(self, get_response: typing.Callable) -> None:
-        if RECORDING_DISABLED:
-            raise MiddlewareNotUsed("UserVisit recording has been disabled")
-        self.get_response = get_response
 
-    def __call__(self, request: HttpRequest) -> typing.Optional[HttpResponse]:
-        if request.user.is_anonymous:
-            return self.get_response(request)
+@sync_to_async
+def aget_user_from_request(request):
+    return request.user if bool(request.user) else None
 
-        if RECORDING_BYPASS(request):
-            return self.get_response(request)
 
-        uv = UserVisit.objects.build(request, timezone.now())
-        if not UserVisit.objects.filter(hash=uv.hash).exists():
-            save_user_visit(uv)
+@sync_and_async_middleware
+def UserVisitMiddleware(
+    get_response: typing.Callable[
+        [HttpRequest],
+        HttpResponse | typing.Coroutine[typing.Any, typing.Any, HttpResponse],
+    ],
+):
+    if RECORDING_DISABLED:
+        raise MiddlewareNotUsed("UserVisit recording has been disabled")
 
-        return self.get_response(request)
+    if inspect.iscoroutinefunction(get_response):
+
+        async def middleware(request: HttpRequest) -> typing.Optional[HttpResponse]:
+            user = await aget_user_from_request(request)
+            if user.is_anonymous:
+                return await typing.cast(typing.Awaitable, get_response(request))
+
+            if RECORDING_BYPASS(request):
+                return await typing.cast(typing.Awaitable, get_response(request))
+
+            uv = UserVisit.objects.build(request, timezone.now())
+            if not await UserVisit.objects.filter(hash=uv.hash).aexists():
+                await asave_user_visit(uv)
+
+            return await typing.cast(typing.Awaitable, get_response(request))
+
+    else:
+
+        def middleware(request: HttpRequest) -> typing.Optional[HttpResponse]:
+            if request.user.is_anonymous:
+                return get_response(request)
+
+            if RECORDING_BYPASS(request):
+                return get_response(request)
+
+            uv = UserVisit.objects.build(request, timezone.now())
+            if not UserVisit.objects.filter(hash=uv.hash).exists():
+                save_user_visit(uv)
+
+            return get_response(request)
+
+    return middleware
